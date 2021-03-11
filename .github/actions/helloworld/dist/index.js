cae99e3415e3f326835df753854923cb85c702af
Zalan Szugyi <zalan.szugyi@prezi.com>
2021-03-11 16:57:47 +0100

    xx

--- .github/actions/helloworld/dist/index.js
+++ .github/actions/helloworld/dist/index.js
@@ -13,7 +13,7 @@ async function main() {
 	try {
 		const repo = core.getInput('repository');
 		const prNum = core.getInput('pr-number');
-		const octokit = new Octokit();
+		const octokit = new Octokit({auth: process.env.PERSONAL_ACESSS_TOKEN});
 		// const octokit = new Octokit({baseUrl: "https://api.github.com/"});
 
 		const response = await octokit.request('GET /repos/{repo}/pulls/{pull_number}/files', {
