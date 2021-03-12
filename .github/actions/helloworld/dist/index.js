5c7187ccb52d7eab3c86e49759538051107eeaea
Zalan Szugyi <zalan.szugyi@prezi.com>
2021-03-12 14:20:26 +0100

    x

--- .github/actions/helloworld/dist/index.js
+++ .github/actions/helloworld/dist/index.js
@@ -35,6 +35,7 @@ async function main() {
 		});
 
 		// console.log("ownersResponse: ", ownersResponse)
+		const buff = new Buffer(ownersResponse.data.content, 'base64');
 		const content = atob(ownersResponse.data.content);
 		console.log("Content: ", content);
 
