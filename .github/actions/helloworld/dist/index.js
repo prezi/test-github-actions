2c3988d4ed1911426f6d015f7144dca4cac848bb
Zalan Szugyi <zalan.szugyi@prezi.com>
2021-04-09 11:07:45 +0200

    add more action

--- .github/actions/helloworld/dist/index.js
+++ .github/actions/helloworld/dist/index.js
@@ -12,33 +12,16 @@ async function main() {
 	console.log("Start action")
 	try {
 		const [owner, repo] = core.getInput('repository').split("/");
-		const prNum = core.getInput('pr-number');
 		const myToken = core.getInput('myToken');
 		const octokit = github.getOctokit(myToken);
-		console.log(`data ${repo}, ${prNum}`);
-		// const octokit = new Octokit({baseUrl: "https://api.github.com/"});
-
-		const response = await octokit.request('GET https://api.github.com/repos/{owner}/{repo}/pulls/{pull_number}/files', {
+		const headCommitSha =
+		github.context.payload.pull_request != null ? github.context.payload.pull_request.head.sha : null;
+		await octokit.request("POST /repos/{owner}/{repo}/statuses/{sha}", {
 			owner: owner,
 			repo: repo,
-			pull_number: prNum
-		})
-
-		for (const r of response.data) {
-			console.log("-", r.filename);
-		}
-
-		const ownersResponse = await octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {
-			owner: owner,
-			repo: repo,
-			path: 'module_a/OWNERS'
+			sha: headCommitSha,
+			state: "success",
 		});
-
-		// console.log("ownersResponse: ", ownersResponse)
-		const buff = Buffer.from(ownersResponse.data.content, 'base64');
-		const content = buff.toString('ascii');
-		console.log("Content: ", content);
-
 
 	} catch (error) {
 		core.setFailed(error.message);
