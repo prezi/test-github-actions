8c322a4f20c3d779ac6c777966ee38d7443086f7
Zalan Szugyi <zalan.szugyi@prezi.com>
2021-04-09 11:24:25 +0200

    log

--- .github/actions/helloworld/index.js
+++ .github/actions/helloworld/index.js
@@ -8,7 +8,8 @@ async function main() {
 		const myToken = core.getInput('myToken');
 		const octokit = github.getOctokit(myToken);
 		const headCommitSha =
-		github.context.payload.pull_request != null ? github.context.payload.pull_request.head.sha : null;
+			github.context.payload.pull_request != null ? github.context.payload.pull_request.head.sha : null;
+		console.log("xxx: owner, repo, commit: ", owner, repo, headCommitSha)
 		await octokit.request("POST /repos/{owner}/{repo}/statuses/{sha}", {
 			owner: owner,
 			repo: repo,
